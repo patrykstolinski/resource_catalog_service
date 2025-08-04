dcb735534f9f2184a0fdc68d1a8d2822a993caef
Patryk Stolinski <patrykstolinski@gmail.com>
2025-08-04 12:59:41 +0200

    Implemented GET /resources/:id endpoint. Added helper function to load data.

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ const {port, hostname} = config;
 
 // load resources.json function
 const resources = path.join(__dirname, "data/resources.json");
+
 // load any JSON data you want.
 async function loadData(data) {
     const file_data = await readFile(data, "utf-8");
     return JSON.parse(file_data);
 }
-
 
 // main node
 app.get("/", (req,res) => {
@@ -38,8 +38,29 @@ app.get("/resources", async (req,res) => {
     }
 });
 
+// /resources/id node
+app.get("/resources/:id", async (req,res) => {    
+    try {        
+        const json = await loadData(resources); // use load function to load resources 
+        const resourceId = req.params.id;// req.params look for key (in this case "id") and assigns the value to resourceId        
+        const resource = json.find(item => item.id === resourceId);// arrow function - for each item, check if item.id is the same as resourceId        
+        if(!resource) {
+            return res.status(404).send(`Resource with ID "${resourceId}" not found.`)
+        }; // if no resource found, throw 404        
+        res.json(resource);// return the single resource 
+
+    } catch (error) {
+        console.error(`Error reading ${resources}`, error);
+        res.status(500).send(`Error loading ${resources}`);
+    }
+});
+
+
+
 app.listen(port, () => {
     console.log(`Server listening at http://${hostname}:${port}`);
     console.log(`Resources GET endpoint: http://${hostname}:${port}/resources`);
+    console.log(`Resources/:id test GET endpoint: http://${hostname}:${port}/resources/1`);
+    console.log(`Resources/:id bad GET endpoint: http://${hostname}:${port}/resources/99`);
 
 });
