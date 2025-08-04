ac623b0c8d7021b4ac7b95f4e651a01ec25dd9a0
Patryk Stolinski <patrykstolinski@gmail.com>
2025-08-04 11:39:07 +0200

    RC-002: Implemented /GET for resources.json. Added config.json for easier configuration.

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 import express from "express";
+import path from "path";
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from "url";
 
 const app = express();
 
-const port = 5002;
-const localhost = "127.0.0.1";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const configPath = path.join(__dirname,"config.json");
 
+// read the config file and add the info to the variables
+const config = JSON.parse(await readFile(configPath, "utf-8"));
+const {port, hostname} = config;
+
+
+// main node
 app.get("/", (req,res) => {
     res.send("Welcome to Resource Catalog");
 });
 
+// /resources node, that reads resources.JSON
+app.get("/resources", async (req,res) => {
+
+    const resources = path.join(__dirname, "data/resources.json");
+    try {
+        const fileData = await readFile(resources, "utf-8");
+        const json = JSON.parse(fileData);
+        res.json(json);
+    } catch (error) {
+        console.error(`Error reading ${resources}`, error);
+        res.status(500).send(`Error loading ${resources}`);
+    }
+});
 
 app.listen(port, () => {
-    console.log(`Server listening at http://${localhost}:${port}`);
+    console.log(`Server listening at http://${hostname}:${port}`);
+    console.log(`Resources GET endpoint: http://${hostname}:${port}/resources`);
 
 });
