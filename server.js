ef2842173900679633ee66531b3f9359417e099b
Patryk Stolinski <patrykstolinski@gmail.com>
2025-08-06 11:32:44 +0200

    RC-011: Added .env file to hold the global variables.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import resourcesRouter from "./routes/resources.js";
+import dotenv from "dotenv";
 
-const port = 5002;
-const hostname = "127.0.0.1";
+dotenv.config();
+const port = process.env.PORT || 5001;
+const hostname = process.env.HOST || "127.0.0.1";
 
 const app = express();
 
@@ -26,7 +28,7 @@ app.use("/resources", resourcesRouter);
 
 app.listen(port, () => {
     console.log(`Server listening at http://${hostname}:${port}`);
-    console.log(`Resources GET endpoint: http://${hostname}:${port}/resources`);
-    console.log(`Test valid GET endpoint for Resources/:id : http://${hostname}:${port}/resources/1`);
-    console.log(`Test INVALID GET endpoint for Resources/:id: http://${hostname}:${port}/resources/99`);
+    // console.log(`Resources GET endpoint: http://${hostname}:${port}/resources`);
+    // console.log(`Test valid GET endpoint for Resources/:id : http://${hostname}:${port}/resources/1`);
+    // console.log(`Test INVALID GET endpoint for Resources/:id: http://${hostname}:${port}/resources/99`);
 });
